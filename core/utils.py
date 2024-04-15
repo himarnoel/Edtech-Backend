76be65752a7534e444f92ea1954647678a3345e8
himarnoel <olaimarnoel>
2024-04-15 02:17:55 +0100

    working

--- core/utils.py
+++ core/utils.py
@@ -5,14 +5,14 @@ from django.utils.encoding import force_bytes
 from django.template.loader import render_to_string
 from rest_api_payload import success_response, error_response
 
-
+baseURL="https://haelsoft-elearning-fe.vercel.app"
 def send_verification_email(email, verification_token):
     subject = 'Verify Your Email Address'
     from_email = settings.EMAIL_HOST_USER  # Use a sender email address from your domain
     to_email = email
     
     # Render email template with context
-    html_content = render_to_string('verification_email.html', {'verification_link': f'http://localhost:8000/auth/api/verify-email/{verification_token}/','useremail': email})
+    html_content = render_to_string('verification_email.html', {'verification_link': f'{baseURL}/auth/api/verify-email/{verification_token}/','useremail': email})
     
     email = EmailMultiAlternatives(subject, '', from_email, [to_email])
     email.attach_alternative(html_content, "text/html")
@@ -26,7 +26,7 @@ def send_resetPassword_email(user, token):
     to_email = user.email
     
     
-    reset_link = f" http://localhost:8000/auth/api/reset-password/{urlsafe_base64_encode(force_bytes(user.pk))}/{token}/"
+    reset_link = f" {baseURL}/auth/api/reset-password/{urlsafe_base64_encode(force_bytes(user.pk))}/{token}/"
     
     html_content = render_to_string("resetpassword.html",{"reset_link":reset_link,'useremail': user.email, "userName":user.fullName})
     
