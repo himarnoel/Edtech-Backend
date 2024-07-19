e78fc6640e150e898a3ee86aad973f2010a79d72
himarnoel <olaimarnoel>
2024-07-19 13:18:48 +0100

    added pagination to the application

--- courses/views.py
+++ courses/views.py
@@ -7,13 +7,14 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from django.http import Http404
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import permissions
 
 # Create your views here.
 
 
-
-
 class BaseCRUDViewSet(viewsets.ModelViewSet):
+    # permission_classes=[permissions.IsAuthenticated]
     """
     Base class for handling POST, PUT, and PATCH,DELETE and GET both single and all requests in one Class.
     """
@@ -23,7 +24,7 @@ class BaseCRUDViewSet(viewsets.ModelViewSet):
         Handles creation (POST), update (PUT), and partial update (PATCH) requests.
         """
         # Determine if the request is a POST or an update request
-        is_post = request.method == 'POST'
+        is_post = request.method == "POST"
         try:
             if is_post:
                 # For POST requests, there is no instance to update
@@ -35,8 +36,7 @@ class BaseCRUDViewSet(viewsets.ModelViewSet):
                 data = request.data
 
             # Create a serializer instance with the data and instance
-            serializer = self.get_serializer(
-                instance, data=data, partial=not is_post)
+            serializer = self.get_serializer(instance, data=data, partial=not is_post)
 
             # Check if the data is valid
             if serializer.is_valid():
@@ -45,8 +45,10 @@ class BaseCRUDViewSet(viewsets.ModelViewSet):
                 status_code = status.HTTP_201_CREATED if is_post else status.HTTP_200_OK
                 # Return a success response with the serializer data
                 payload = success_message(
-                    message="Created Successfully" if is_post else "Updated Successfully",
-                    data=serializer.data
+                    message=(
+                        "Created Successfully" if is_post else "Updated Successfully"
+                    ),
+                    data=serializer.data,
                 )
                 return Response(data=payload, status=status_code)
 
@@ -66,8 +68,6 @@ class BaseCRUDViewSet(viewsets.ModelViewSet):
         except Exception as e:
             payload = error_message(message="An error occurred")
             return Response(data=payload, status=status.HTTP_400_BAD_REQUEST)
-        
-        
 
     def destroy(self, request, *args, **kwargs):
         # Attempt to retrieve the object to be deleted
@@ -75,63 +75,77 @@ class BaseCRUDViewSet(viewsets.ModelViewSet):
             instance = self.get_object()
             # Perform the delete operation
             self.perform_destroy(instance)
-            payload = success_message(
-                message="Deleted successfully", data="")
+            payload = success_message(message="Deleted successfully", data="")
             # Return a success response with a 204 status code
             return Response(data=payload, status=status.HTTP_204_NO_CONTENT)
 
         except NotFound:
-            payload = error_message(
-                message="Id not found")
+            payload = error_message(message="Id not found")
             return Response(data=payload, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
             # Handle any unexpected exceptions that may occur
-            payload = error_message(
-                message="An error occurred during deletion")
+            payload = error_message(message="An error occurred during deletion")
             return Response(data=payload, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_paginated_response(self, data):
+        """
+        Pagination
+        """
+        paginator = self.paginator
+        if paginator is None:
+            paginator = PageNumberPagination()
+        page = paginator.page
+        pagination_metadata = {
+            "pageNumber": page.number,
+            "pageSize": paginator.page_size,
+            "totalPages": paginator.page.paginator.num_pages,
+            "totalRecords": paginator.page.paginator.count,
+        }
+
+        # Create paginated response payload
+        payload = success_message(
+            message="Fetched successfully",
+            data={"pageMeta": pagination_metadata, "results": data},
+        )
+        return Response(data=payload, status=status.HTTP_200_OK)
 
     def list(self, request, *args, **kwargs):
         """
         Retrieve a list of objects from the queryset.
         """
-
         queryset = self.filter_queryset(self.get_queryset())
-
-        # Paginate the queryset if required
         page = self.paginate_queryset(queryset)
+
         if page is not None:
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
-
-        # If no pagination is required, serialize and return the full queryset
-        serializer = self.get_serializer(queryset, many=True)
-        payload = success_message(
-            message="Fetched successfully", data=serializer.data)
-        return Response(data=payload, status=status.HTTP_200_OK)
+        else:
+            serializer = self.get_serializer(queryset, many=True)
+            payload = success_message(
+                message="Fetched successfully", data=serializer.data
+            )
+            return Response(data=payload, status=status.HTTP_200_OK)
 
     def retrieve(self, request, *args, **kwargs):
         try:
             instance = self.get_object()
             serializer = self.get_serializer(instance)
             payload = success_message(
-                message="Fetched successfully", data=serializer.data)
+                message="Fetched successfully", data=serializer.data
+            )
             return Response(data=payload, status=status.HTTP_200_OK)
         except NotFound:
             payload = error_message(message="ID not found")
             return Response(data=payload, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
-            payload = error_message(
-                message="An error occurred during retrieval.")
+            payload = error_message(message="An error occurred during retrieval.")
             return Response(data=payload, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
 
 
 class CategoryViewSet(BaseCRUDViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    
 
     # You can call handle_create_update for create, update, and partial_update methods.
     def create(self, request, *args, **kwargs):
@@ -161,6 +175,7 @@ class SubcategoryViewSet(BaseCRUDViewSet):
 class CourseViewSet(BaseCRUDViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+   
 
     def create(self, request, *args, **kwargs):
         return self.handle_create_update(request, *args, **kwargs)
--- edtechbackend/settings.py
+++ edtechbackend/settings.py
@@ -174,9 +174,9 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # settings.py
-
-# REST_FRAMEWORK = {
-#     'EXCEPTION_HANDLER': 'edtechbackend.utils.custom_exception_handler'
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,  # Number of items per page
+}
 
 SWAGGER_SETTINGS = {"SECURITY_DEFINITIONS": {"Basic": {"type": "basic"}}}
