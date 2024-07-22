48be368f9b97d2c0127736b1142ae31fab97229e
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-07-22 16:41:12 +0100

    firebase

--- core/firebase.py
+++ core/firebase.py
@@ -1,5 +1,6 @@
 import firebase_admin
+import os
 from firebase_admin import credentials
 
-cred = credentials.Certificate('secrets/firebase-credentials.json')
+cred = credentials.Certificate(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
 firebase_admin.initialize_app(cred)
