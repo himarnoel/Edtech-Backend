7863709385e0f11e7c853a29f8f90860b41d868e
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-07-29 01:44:57 +0100

    removed firebase

--- core/firebase.py
+++ core/firebase.py
@@ -1,6 +1,6 @@
-import firebase_admin
-import os
-from firebase_admin import credentials
+# import firebase_admin
+# import os
+# from firebase_admin import credentials
 
-cred = credentials.Certificate(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
-firebase_admin.initialize_app(cred)
+# cred = credentials.Certificate(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
+# firebase_admin.initialize_app(cred)
