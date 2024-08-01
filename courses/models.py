625077c2201be5d6c97b4c4564578ddf08c215d6
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-08-01 10:34:18 +0100

    removed subcategory table

--- courses/models.py
+++ courses/models.py
@@ -16,18 +16,6 @@ class Category(models.Model):
 
 
 
-class Subcategory(models.Model):
-    subcategory_id = models.UUIDField(default=uuid.uuid4,  primary_key=True, editable=False, unique=True)
-    name = models.CharField(max_length=100,unique=True)
-    description = models.TextField(max_length=200)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
-    
-    def __str__(self):
-        return self.name
-
-
-
-
 class Course(models.Model):
     course_id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False, unique=True)
     title = models.CharField(max_length=200)
@@ -35,7 +23,7 @@ class Course(models.Model):
     description = models.TextField(max_length=800,unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     creation_date = models.DateTimeField(auto_now_add=True)
-    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE, related_name='course')
+   
     
     def __str__(self):
         return self.title
