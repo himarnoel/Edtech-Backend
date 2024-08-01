95bf7da7e561335d66d60282eed5456df410b738
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-08-01 10:33:55 +0100

    removed subcategroies

--- courses/urls.py
+++ courses/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, SubcategoryViewSet, CourseViewSet
+from .views import CategoryViewSet,  CourseViewSet
 
 # Create a router and register your viewsets
 router = DefaultRouter()
 router.register(r'category', CategoryViewSet, basename='category')
-router.register(r'subcategory', SubcategoryViewSet, basename='subcategory')
 router.register(r'course', CourseViewSet, basename='course')
 
 urlpatterns = router.urls
