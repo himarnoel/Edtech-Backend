d150ac38e16cf012b04bedcd727f4cced5313503
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-08-01 11:27:42 +0100

    added course reviews

--- courses/urls.py
+++ courses/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet,  CourseViewSet
+from .views import CategoryViewSet,  CourseViewSet, CourseReviewViewSet
 
 # Create a router and register your viewsets
 router = DefaultRouter()
 router.register(r'category', CategoryViewSet, basename='category')
 router.register(r'course', CourseViewSet, basename='course')
+router.register(r'coursereviews', CourseReviewViewSet, basename='coursereviews')
 
 urlpatterns = router.urls
